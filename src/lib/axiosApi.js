0ddaf96912f2b45808f2ff74b53f25ea1442f518
Robinsonsarangthem <robinsonsarangthem33@gmail.com>
2024-10-26 16:41:52 +0530

    updated baseUrl

--- src/lib/axiosApi.js
+++ src/lib/axiosApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const url = 'http://achaathak.com/backend/'
+export const url = 'https://achaathak.com/backend'
 // export const url = 'https://6xmlzdw3-4000.inc1.devtunnels.ms/'
 export const Axios = axios.create({
 	baseURL: url,
