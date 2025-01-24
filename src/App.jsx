c077f4ed1bd522aee5ee8848227230c9988b40d4
nganaremba-rem <ngancloverr+remkugithub@gmail.com>
2025-01-24 23:05:13 +0530

    basename /admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,46 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import UploadItem from './components/pages/UploadItem'
-import FoodList from './components/pages/components/FoodList/FoodList'
-import Orders from './components/order/Orders'
-import Layout from './components/Layout/Layout'
-import WelcomePage from './components/pages/components/WelcomePage'
-import Login from './components/LoginPage/Login'
-import SignUp from './components/LoginPage/SignUp'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import { AuthProvider } from './components/context/AuthContext'
-import ProtectedRoute from './components/element/ProtectedRoute'
-import Profile from './components/header/Profile'
-import CheckIfAlreadyAuthenticated from './components/CheckIfAlreadyAuthenticated'
-import Update from './components/pages/components/FoodList/Update'
-import PageNotFound from './components/pages/PageNotFound'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import UploadItem from './components/pages/UploadItem';
+import FoodList from './components/pages/components/FoodList/FoodList';
+import Orders from './components/order/Orders';
+import Layout from './components/Layout/Layout';
+import WelcomePage from './components/pages/components/WelcomePage';
+import Login from './components/LoginPage/Login';
+import SignUp from './components/LoginPage/SignUp';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import { AuthProvider } from './components/context/AuthContext';
+import ProtectedRoute from './components/element/ProtectedRoute';
+import Profile from './components/header/Profile';
+import CheckIfAlreadyAuthenticated from './components/CheckIfAlreadyAuthenticated';
+import Update from './components/pages/components/FoodList/Update';
+import PageNotFound from './components/pages/PageNotFound';
 
 function App() {
-	return (
-		<div className='bg-slate-300 min-h-screen'>
-			<ToastContainer />
-			<AuthProvider>
-				<Routes>
-					<Route element={<ProtectedRoute />}>
-						<Route element={<Layout />}>
-							<Route index element={<WelcomePage />} />
-							<Route path='/add' element={<UploadItem />} />
-							<Route path='/list' element={<FoodList />} />
-							<Route path='/orders' element={<Orders />} />
-							<Route path='/profile' element={<Profile />} />
-							<Route path='/update/:id' element={<Update />} />
-						</Route>
-					</Route>
-					<Route path='*' element={<PageNotFound />} />
-					<Route element={<CheckIfAlreadyAuthenticated />}>
-						<Route path='/sign_up' element={<SignUp />} />
-						<Route path='/sign_in' element={<Login />} />
-					</Route>
-				</Routes>
-			</AuthProvider>
-		</div>
-	)
+  return (
+    <div className='bg-slate-300 min-h-screen'>
+      <ToastContainer />
+      <AuthProvider>
+        <Routes>
+          <Route element={<ProtectedRoute />}>
+            <Route element={<Layout />}>
+              <Route index element={<WelcomePage />} />
+              <Route path='/add' element={<UploadItem />} />
+              <Route path='/list' element={<FoodList />} />
+              <Route path='/orders' element={<Orders />} />
+              <Route path='/profile' element={<Profile />} />
+              <Route path='/update/:id' element={<Update />} />
+            </Route>
+          </Route>
+          <Route element={<CheckIfAlreadyAuthenticated />}>
+            <Route path='/sign_up' element={<SignUp />} />
+            <Route path='/sign_in' element={<Login />} />
+          </Route>
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </AuthProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/lib/axiosApi.js
+++ src/lib/axiosApi.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export const url = 'https://achaathak.com/backend'
+export const url = 'https://achaathak.com/backend';
 // export const url = 'https://6xmlzdw3-4000.inc1.devtunnels.ms/'
 export const Axios = axios.create({
-	baseURL: url,
-})
+  baseURL: url,
+});
