511ee9d907c35a0e43ff9f1831786ebbb19cac24
nganaremba-rem <ngancloverr+remkugithub@gmail.com>
2025-01-25 19:25:06 +0530

    removed handling the /admin path, changed vite config and package json

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { AuthProvider } from './components/context/AuthContext.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthProvider } from './components/context/AuthContext.jsx';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<BrowserRouter basename='/admin'>
-		<QueryClientProvider client={queryClient}>
-			<App />
-		</QueryClientProvider>
-	</BrowserRouter>
-)
+  <BrowserRouter basename='/admin'>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </BrowserRouter>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
-	base: '/admin',
-	plugins: [react()],
-	build: {
-		sourcemap: false, // Disable sourcemaps for production builds
-	},
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url)),
-		},
-	},
-})
+  base: '/admin',
+  plugins: [react()],
+  build: {
+    outDir: 'admin',
+    sourcemap: false, // Disable sourcemaps for production builds
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
